bd1a441a8a55a06bd97b596d4eeacf3121e5c173
MasatoArima <afoc.0707@gmail.com>
2021-10-19 06:43:00 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
     get "/about", to: "homes#about"
     get "/customer/withdraw_confirm", to: "customers#withdraw_confirm"
     patch "/customer/withdraw", to: "customers#withdraw"
-    resource :customer, only:[:show, :edit, :update]
+    resource :customer, only:[:show, :update]
     resources :items, only:[:index, :show]
     resources :cart_items, except:[:show, :new, :edit]
     delete "/cart_items", to: "cart_items#destroy_all"
