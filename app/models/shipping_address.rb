985e1b1d0a5d49cebee6b60b2dc5e2a64121aa84
ayakawamura <ayakawamura.b@gmail.com>
2021-10-19 01:42:17 +0000

    [add]モデルにvalidates追記

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,8 @@
 class ShippingAddress < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :post_code, presence: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,17 @@ Admin.create!(
   email: 'test@test',
   password: 'testtest'
   )
+
+15.times do |n|
+  Customer.create!(
+    email: "test#{n + 1}@test.com",
+    password: "111111",
+    last_name: "test",
+    first_name: "#{n + 1}",
+    last_name_kana: "テスト",
+    first_name_kana: "#{n + 1}",
+    address: "a県",
+    post_code: "0000000",
+    phone_number: "11111111",
+  )
+end
