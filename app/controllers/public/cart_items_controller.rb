2cfa7e1a2660150805616cf62c35733457c824f1
ayakawamura <ayakawamura.b@gmail.com>
2021-10-20 02:33:23 +0000

    [Add]カート機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,37 @@
 class Public::CartItemsController < ApplicationController
-  def index
+
+  def create
+    @cart_item = current_customer.cart_items.build(item_id: params[:item_id])
+    @cart_item.quantity = params[:quantity].to_i
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      redirect_to request.referer
+    end
   end
+  
+  
+  def index
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+  end
+  
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(quantity: params[:quantity].to_i)
+    redirect_to cart_items_path
+  end
+  
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
+  end
+  
+  def destroy_all
+    @cart_items = current_customer.cart_items.all
+    @cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+  
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,12 @@
 class CartItem < ApplicationRecord
+  belongs_to :item
+  belongs_to :customer
+  
+  validates :quantity, presence: true
+  
+  # カート内の商品合計に利用
+  def sum_of_price
+    item.add_tax_included_price * quantity
+  end
+
 end
