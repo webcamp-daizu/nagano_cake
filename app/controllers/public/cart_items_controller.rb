41e571a5800eed531eff99e1081acc60b1efd661
ayakawamura <ayakawamura.b@gmail.com>
2021-10-23 12:23:28 +0000

    [Add]cardレイアウト

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -18,19 +18,19 @@ class Public::CartItemsController < ApplicationController
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
-    redirect_to cart_items_path
+    redirect_to cart_items_path, notice: '数量を変更しました'
   end
 
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
-    redirect_to cart_items_path
+    redirect_to cart_items_path, notice: '商品を削除しました'
   end
 
   def destroy_all
     @cart_items = current_customer.cart_items.all
     @cart_items.destroy_all
-    redirect_to cart_items_path
+    redirect_to cart_items_path,notice: '全ての商品を削除しました'
   end
 
   def cart_item_params
